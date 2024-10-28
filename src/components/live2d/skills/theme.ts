ef0217716307f794b419a066ab6ec85766a21300
李耀 <liyao@uzoo.cn>
2024-10-28 16:43:07 +0800

    fix: 调用时机

--- src/components/live2d/skills/theme.ts
+++ src/components/live2d/skills/theme.ts
@@ -7,12 +7,13 @@ let timer: NodeJS.Timeout
 export function toggleTheme(live2dView: Live2DModelView, isDark: Ref<boolean>) {
 
   const { model, app } = live2dView
-  const { left, top } = app.view.getBoundingClientRect()
+
 
   const config = isDark.value ? { motionIndex: 3, delay: 3500 } : { motionIndex: 4, delay: 5000 }
   model.motion('', config.motionIndex)
   clearTimeout(timer)
   timer = setTimeout(() => {
+    const { left, top } = app.view.getBoundingClientRect()
     nextTick(() => startTransition(isDark, {
       x: left + 50,
       y: top + 50
